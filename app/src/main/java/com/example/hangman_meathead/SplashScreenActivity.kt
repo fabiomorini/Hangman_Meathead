db5085a41515e2c6ebe94aa5b1ebe5f88f338886
Adrián <jagv94@gmail.com>
2022-11-25 17:10:46 +0100

    Binding fixed

--- app/src/main/java/com/example/hangman_meathead/SplashScreenActivity.kt
+++ app/src/main/java/com/example/hangman_meathead/SplashScreenActivity.kt
@@ -3,19 +3,20 @@ package com.example.hangman_meathead
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.hangman_meathead.databinding.ActivitySplashScreenBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
-    private lateinit var binding: SplashScreenActivity
+    private lateinit var binding: ActivitySplashScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //binding = SplashScreenActivity.
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
 
-        //setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_splash_screen)
 
         CoroutineScope(Dispatchers.Default).launch {
             delay(3000)
