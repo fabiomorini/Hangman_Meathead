fa1e8123fa9a6d22c311fc5f0a406d9e2feb499d
Fabio Morini <fabio.morini@enti.cat>
2022-11-25 16:56:55 +0100

    Binding

--- app/src/main/java/com/example/hangman_meathead/SplashScreenActivity.kt
+++ app/src/main/java/com/example/hangman_meathead/SplashScreenActivity.kt
@@ -1,11 +1,29 @@
 package com.example.hangman_meathead
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: SplashScreenActivity
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        //binding = SplashScreenActivity.
+
+        //setContentView(R.layout.activity_splash_screen)
+
+        CoroutineScope(Dispatchers.Default).launch {
+            delay(3000)
+
+            val intentMain = Intent(this@SplashScreenActivity,  MainActivity::class.java)
+            startActivity(intentMain)
+
+            finish()
+        }
     }
 }
