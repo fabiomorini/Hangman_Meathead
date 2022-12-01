d8933a67472273bf0ad1cd33b00f0eeeefb3c65a
Adrián <jagv94@gmail.com>
2022-12-02 00:43:35 +0100

    Improving Sign Up Layout

---
+++ app/src/main/java/com/example/hangman_meathead/SignUpActivity.kt
@@ -0,0 +1,10 @@
+package com.example.hangman_meathead
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
