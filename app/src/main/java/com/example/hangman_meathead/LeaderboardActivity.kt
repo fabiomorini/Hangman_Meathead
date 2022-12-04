f7c3e7ae88535f3f346d9f26dedc6475d41e4e2d
Fabio Morini <fabio.morini@enti.cat>
2022-12-04 15:46:03 +0100

    setup adapter and recycler view

--- app/src/main/java/com/example/hangman_meathead/LeaderboardActivity.kt
+++ app/src/main/java/com/example/hangman_meathead/LeaderboardActivity.kt
@@ -5,39 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.hangman_meathead.databinding.ActivityLeaderboardBinding
 
 class LeaderboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityLeaderboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recycler.adapter = ScoreAdapter()
     }
-}
-
-class CustomRecyclerViewAdapter(private val elements: List<Element>) :
-
-    RecyclerView.Adapter<CustomRecyclerViewAdapter.ElementViewHolder>() {
-
-    inner class ElementViewHolder(binding: ListElementBinding) : RecyclerView.ViewHolder(binding.root) {
-        //set the variables for our element
-        val name = binding.name
-        val image = binding.image
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ElementViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val binding = ItemListParticleBinding.inflate(layoutInflater, parent, false)
-        return ParticleViewHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: ElementViewHolder, position: Int) {
-        val element = elements[position]
-
-        // Set name and image
-        holder.name.text = element.name
-        holder.image.drawable = element.image
-    }
-
-
-    override fun getItemCount(): Int = elements.size
-
-
 }
