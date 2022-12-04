a55382acbdeb788302cfa5f7e5c6ff8371a8fe17
Fabio Morini <fabio.morini@enti.cat>
2022-12-04 14:38:02 +0100

    added leaderbpard screen + design

---
+++ app/src/main/java/com/example/hangman_meathead/LeaderboardActivity.kt
@@ -0,0 +1,10 @@
+package com.example.hangman_meathead
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeaderboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
