e69bd1f317ed594ebd3ea789e06bde11266a02fc
Antonio <antonio.robreno@enti.cat>
2022-12-04 20:03:43 +0100

    LederboardButtonAndName

--- app/src/main/java/com/example/hangman_meathead/LeaderboardActivity.kt
+++ app/src/main/java/com/example/hangman_meathead/LeaderboardActivity.kt
@@ -1,5 +1,6 @@
 package com.example.hangman_meathead
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -16,7 +17,7 @@ class LeaderboardActivity : AppCompatActivity() {
         binding.recycler.adapter = ScoreAdapter(
             listOf(
                 Score("Shiroi_Okami", "Points: 123000","url1"),
-                Score("SoniKkV2", "Points: 12300","url2"),
+                Score("SonikV2", "Points: 12300","url2"),
                 Score("SKAAARRRL", "Points: 1230","url3"),
                 Score("Manolo69", "Points: 103","url4"),
                 Score("Boji", "Points: 23","url5"),
@@ -24,5 +25,13 @@ class LeaderboardActivity : AppCompatActivity() {
                 Score("Ruth1209", "Points: 3","url7")
             )
         )
+
+        binding.exitButtonLeaderboard.setOnClickListener{
+            val intentMain = Intent(this@LeaderboardActivity, MainMenuActivity::class.java)
+            startActivity(intentMain)
+
+            finish()
+        }
+
     }
 }
