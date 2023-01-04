4f527fdb5e5789355ad5da9e1daadf5b91adef0e
Adrián <jagv94@gmail.com>
2023-01-04 14:59:56 +0100

    Fixes

--- app/src/main/java/com/example/hangman_meathead/LoginActivity.kt
+++ app/src/main/java/com/example/hangman_meathead/LoginActivity.kt
@@ -82,28 +82,4 @@ class LoginActivity : AppCompatActivity() {
             finish()
         }
     }
-
-    override fun onStart() {
-        super.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
 }
